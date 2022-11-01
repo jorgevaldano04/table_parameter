b176b75b682aa7ca8b1ed5ad2e2ddd1cbb30f089
unknown <jorgevaldano04@gmail.com>
2022-11-01 07:57:04 +0700

    pertama

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h2 class="mb-4" style="color:#008080;">Daftar Parameter</h2> 
+      <parameter-table :parameters='parameters'/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ParameterTable from '@/components/ParameterTable.vue'
+
+
+export default {
+  name: "App",
+  data() {
+    return {
+      parameters: []
+    };
+  },
+  mounted() {
+    this.getParameters();
+  },
+  components: {
+    ParameterTable
+  },
+  methods: {
+    async getParameters() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        this.parameters = data;
+      } catch (error) {
+        console.error(error);
+      }
+    }, 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ParameterTable.vue
@@ -0,0 +1,68 @@
+<template>
+<tr>
+  <div class="form-row">
+    <div class="col">
+      <input class="form-control mr-sm-2" type="search" placeholder="Fillter Nama Parameter" aria-label="Search">
+    </div>
+    <div class="col">
+      <input class="form-control mr-sm-2" type="search" placeholder="Fillter Value" aria-label="Search">
+    </div>
+  </div>
+</tr>
+
+
+
+
+<table class="table table-striped">
+
+
+     <thead class="thead-light">
+       <tr class="table-secondary"> 
+         <th scope="col">No</th>
+         <th scope="col">Nama Parameter</th>
+         <th scope="col">Value</th>  
+         <th scope="col">Aksi</th>
+       </tr>
+
+     </thead>
+     <tbody>
+   <tr v-for="(parameter, index) in parameters" :key="parameter.id"> 
+     <td>{{ index + 1 }}</td>
+     <td>{{ parameter.name }}</td>
+     <td>{{ parameter.username }}</td>
+     <td><button type="button" class="btn btn-warning">Detail</button></td>
+   </tr>  
+ </tbody>
+   </table>
+   <nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-end">
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1">Sebelumnya</a>
+      
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Selanjutnya</a>
+    </li>
+  </ul>
+</nav>
+
+
+  
+   </template>
+   
+   <script>
+      export default {
+     name: 'parameter-table',
+     props: {
+       parameters: Array,
+     },
+     
+   }
+   </script>
+   
+   <style>
+   
+   </style>
